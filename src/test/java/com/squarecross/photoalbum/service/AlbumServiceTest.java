4325a2f34a47d4ddea5bdb3328d17147afa23290
jspark <98972385+JunseoParKK@users.noreply.github.com>
2023-04-11 18:12:18 +0900

    Add AlbumServiceTest

--- src/test/java/com/squarecross/photoalbum/service/AlbumServiceTest.java
+++ src/test/java/com/squarecross/photoalbum/service/AlbumServiceTest.java
@@ -13,6 +13,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -86,6 +89,30 @@ class AlbumServiceTest {
         assertThat(photoRepository.countByAlbum_AlbumId(saveAlbum.getAlbumId()))
                 .isEqualTo(1);
 
+    }
+
+    @Test
+    void testAlbumRepository() throws InterruptedException{
+        Album album1 = new Album();
+        Album album2 = new Album();
+        album1.setAlbumName("aaaa");
+        album2.setAlbumName("aaab");
+
+        albumRepository.save(album1);
+        TimeUnit.SECONDS.sleep(1); //시간차를 벌리기위해 두번째 앨범 생성 1초 딜레이
+        albumRepository.save(album2);
+
+        //최신순 정렬, 두번째로 생성한 앨범이 먼저 나와야한다.
+        List<Album> resDate = albumRepository.findByAlbumNameContainingOrderByCreatedAtDesc("aaa");
+        assertEquals("aaab", resDate.get(0).getAlbumName()); // 0번째 Index가 두번째 앨범명 aaab 인지 체크
+        assertEquals("aaaa", resDate.get(1).getAlbumName()); // 1번째 Index가 첫번째 앨범명 aaaa 인지 체크
+        assertEquals(2, resDate.size()); // aaa 이름을 가진 다른 앨범이 없다는 가정하에, 검색 키워드에 해당하는 앨범 필터링 체크
+
+        //앨범명 정렬, aaaa -> aaab 기준으로 나와야한다.
+        List<Album> resName = albumRepository.findByAlbumNameContainingOrderByAlbumNameAsc("aaa");
+        assertEquals("aaaa", resName.get(0).getAlbumName()); // 0번째 Index가 두번째 앨범명 aaaa 인지 체크
+        assertEquals("aaab", resName.get(1).getAlbumName()); // 1번째 Index가 두번째 앨범명 aaab 인지 체크
+        assertEquals(2, resName.size()); // aaa 이름을 가진 다른 앨범이 없다는 가정하에, 검색 키워드에 해당하는 앨범 필터링 체크
     }
 
 }
